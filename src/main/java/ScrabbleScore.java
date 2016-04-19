a6e3047acff80ed0ab337ab31621394414ce129a
ming wen and scott mcintire <mwen82@gmail.com>
2016-04-19 08:29:00 -0700

    Initial commit

---
+++ src/main/java/ScrabbleScore.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class ScrabbleScore {
+  public static void main(String[] args) {}
+
+  public Boolean isScrabbleScore(String input) {
+    return false;
+  }
+}
---
+++ src/test/java/ScrabbleScoreTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ScrabbleScoreTest {
+
+  @Test
+  public void isScrabbleScore_forLetterValue1_true() {
+    
+  }
+}
