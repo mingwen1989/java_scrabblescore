1a39ec3c0dfcecfdf906aec045a79b4cf4966cfc
ming wen and scott mcintire <mwen82@gmail.com>
2016-04-19 10:06:30 -0700

    add single letter values of 3 for scrabble word

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -9,10 +9,22 @@ public class ScrabbleScore {
   public static Integer calculateScore(String word) {
     char[] inputArray = word.toUpperCase().toCharArray();
     HashMap <Character, Integer> scrabbleArray = new HashMap <Character, Integer>();
+    Integer value = 0;
 
     scrabbleArray.put('A', 1);
+    scrabbleArray.put('E', 1);
+    scrabbleArray.put('I', 1);
+    scrabbleArray.put('O', 1);
+    scrabbleArray.put('U', 1);
+    scrabbleArray.put('L', 1);
+    scrabbleArray.put('N', 1);
+    scrabbleArray.put('R', 1);
+    scrabbleArray.put('S', 1);
+    scrabbleArray.put('T', 1);
+    scrabbleArray.put('D', 2);
+    scrabbleArray.put('G', 2);
+    scrabbleArray.put('B', 3);
 
-    Integer value = 0;
     for (char letter : inputArray){
       value += scrabbleArray.get(letter);
       }
--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -9,4 +9,16 @@ public class ScrabbleScoreTest {
     Integer expected = 1;
     assertEquals(expected, testScrabble.calculateScore("A"));
   }
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_2() {
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 2;
+    assertEquals(expected, testScrabble.calculateScore("D"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_3() {
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 3;
+    assertEquals(expected, testScrabble.calculateScore("B"));
+  }
 }
